087bcf37c251ced4602b6aeda2700a326d874e0c
Evgeniy Grakov <grakov-post@yandex.ru>
2022-03-03 22:16:54 +0300

    Fix start command

--- shadowsocks/start.sh
+++ shadowsocks/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t ss_service_image .
+docker build -t ss_service_image -f Dockerfile .
 docker run -d -p 80:80 -p 443:443 --name ss_service_container --restart=always ss_service_image

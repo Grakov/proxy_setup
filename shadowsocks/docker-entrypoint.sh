e34b2b2cd58e72ac869b802615512ab3cc5bf450
Evgeniy Grakov <grakov-post@yandex.ru>
2022-03-03 23:21:37 +0300

    Fix commands

--- shadowsocks/docker-entrypoint.sh
+++ shadowsocks/docker-entrypoint.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-if ! [ -x "$(whereis certbot | cut -d' ' -f2)" ]; then
-  service snapd start
-  snap install core
-  snap refresh core
-  snap install --classic certbot
-  ln -s /snap/bin/certbot /usr/bin/certbot
-fi
-
 nginx
 
-certbot --non-interactive --agree-tos \
+certbot -n --agree-tos \
 -m admin@example.com \
 --no-redirect \
 --email 'admin@example.com' \
---installer nginx \
+--nginx \
 --domains "example.com"
 
 ./wait-for-it.sh -t 0 127.0.0.1:443 -- echo "Certbot done"

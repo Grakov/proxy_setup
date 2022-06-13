91d6fd0099b918e725407202cdba6d828c2e1aed
Evgeniy Grakov <grakov-post@yandex.ru>
2022-06-13 12:47:49 +0300

    Add certificates auto renew

--- shadowsocks/docker-entrypoint.sh
+++ shadowsocks/docker-entrypoint.sh
@@ -9,6 +9,12 @@ certbot -n --agree-tos \
 --nginx \
 --domains "example.com"
 
+crontask=$(cat /etc/crontab | grep 'certbot renew')
+if [[ -z "$crontask"]]; then
+  SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}');
+  echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+fi
+
 ./wait-for-it.sh -t 0 127.0.0.1:443 -- echo "Certbot done"
 /usr/bin/ssserver --log-without-time -c /etc/shadowsocks-rust/config.json
 

5995ef863b3eef7ff462f59256703e7855f9fd6e
Evgeniy Grakov <grakov-post@yandex.ru>
2022-06-13 12:49:59 +0300

    Remove sudo usage

--- shadowsocks/docker-entrypoint.sh
+++ shadowsocks/docker-entrypoint.sh
@@ -12,7 +12,7 @@ certbot -n --agree-tos \
 crontask=$(cat /etc/crontab | grep 'certbot renew')
 if [[ -z "$crontask"]]; then
   SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}');
-  echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+  echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontab > /dev/null
 fi
 
 ./wait-for-it.sh -t 0 127.0.0.1:443 -- echo "Certbot done"
